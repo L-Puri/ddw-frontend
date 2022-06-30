7dd204b40e2ea013edaafd496f6b8a0b0d431b9f
Miguel Sousa Machado <mikesmachado@gmail.com>
2022-06-30 15:13:49 +0100

    login link test

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -4,6 +4,7 @@ import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { AuthContext } from '../context/auth.context'
 import { Link } from "react-router-dom";
+import { BASE_API_URL } from '../utils/constants'
 
 function LoginForm() {
 
